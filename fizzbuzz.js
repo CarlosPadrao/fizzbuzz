e712d31cfd0bc328cbcd5336b1bf3e3ad0c78de1
Carlos Padrão <carlos.padrao92@gmail.com>
2023-09-20 23:18:28 -0300

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,18 @@
+// /3  => Fizz
+// /5 => Buzz
+// /3 /5 => FizzBuzz
+// not /3 or /5 => input
+// not a number => 'Não é um número'
 
+function fizzBuzz(input) {
+  if (typeof input != 'number')
+    return 'Não é um número';
+  if (input % 3 === 0) && (input % 5 === 0)
+    return 'FizzBuzz';
+  if (input % 3 === 0)
+    return 'Fizz';
+  if (input % 5 ===0)
+    return 'Buzz';
+
+  return input;
+}
